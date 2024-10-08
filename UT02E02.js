cd76e2c15fd2db40aaf622602b48e359deba4d68
Lierni Anguita <liernia.anggor@educa.jcyl.es>
2024-10-08 13:53:02 +0200

    first commit

---
+++ UT02E02.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+Se pide hacer una aplicación web con Javascript que pida al usuario un número de DNI con letra y determine si la letra es correcta. 
+Para ello crea un index.html que llame al archivo Javascript con tu código, y utiliza las funciones prompt() y alert() para comunicarte con 
+el usuario.
+Haz este ejercicio en un repositorio git nuevo y súbelo a GitHub.
+Para marcar esta tarea como entregada, indica la URL del repositorio GitHub donde está el código.
+COMO HACER: HTML:referencia al archivo script src="dni.js">>/script> y alguna referencia de aclaracion en el body
+            JS: crear array con las letras de dni
+            crear funcion
+            let dni=prompt("introduce tu dni y letra");
+            verificacion dni(por longitud, por letra)si algo esta mal tenemos que enviar una alerta al usuario=alert("incorrecto")return;
+            toUpperCase(), isNan
+
+            formula= let letra=numero%23;
+
+
+   
+*/
+
+;
