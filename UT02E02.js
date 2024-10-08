5abead62da2bfb213d09c39ea9cb7912f263bc95
Lierni Anguita <liernia.anggor@educa.jcyl.es>
2024-10-08 18:44:17 +0200

    segundo commit

--- UT02E02.js
+++ UT02E02.js
@@ -5,17 +5,46 @@ Para ello crea un index.html que llame al archivo Javascript con tu código, y u
 el usuario.
 Haz este ejercicio en un repositorio git nuevo y súbelo a GitHub.
 Para marcar esta tarea como entregada, indica la URL del repositorio GitHub donde está el código.
-COMO HACER: HTML:referencia al archivo script src="dni.js">>/script> y alguna referencia de aclaracion en el body
-            JS: crear array con las letras de dni
-            crear funcion
-            let dni=prompt("introduce tu dni y letra");
-            verificacion dni(por longitud, por letra)si algo esta mal tenemos que enviar una alerta al usuario=alert("incorrecto")return;
-            toUpperCase(), isNan
-
-            formula= let letra=numero%23;
-
-
-   
 */
 
-;
+function confirmaDniUsuario(dniUsuario = prompt("Introduce tu dni y letra")) {
+    const letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+
+    if (!dniUsuario || dniUsuario.length < 8 || dniUsuario.length > 9) {
+        alert("El DNI no es válido. por longitud");
+        return;
+    }
+    // TENEMOS QUE SEPARAR EL NUMERO DEL DNI DE LA LETRA. funcion slice(1,2) coge los elementos entre los dos parametros desde el primero hasta el ultimo menos 1.
+    let numero = dniUsuario.slice(0, 8); //el ultimo numero no lo coge
+    let letra = dniUsuario.slice(8).toUpperCase();
+
+    if (isNaN(numero)) {
+        alert("El número de DNI no es válido");
+        return;
+    }
+
+    let resto = (numero % 23);
+    //Mirar a partir de aqui
+    let letraResto = "";
+    for (let i = 0; i < letras.length; i++) {
+        if (resto === i) {
+            letraResto = letras[i];
+        }
+    }
+
+    if (letra === letraResto) {//Comparamos letra que nos han dado, con letraDNI la letra correcta despues de hallar el resto
+        alert("El DNI es correcto.");
+        return;
+    } else {
+        alert("La letra del DNI no es correcta. La letra correcta es: " + letraResto);
+        return;
+    }
+}
+
+confirmaDniUsuario();
+
+
+
+
+
+
